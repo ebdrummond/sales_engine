aa063a3471160fd5b0a0bf1faad1a119344206c5
erindrummond <e.b.drummond@gmail.com>
2013-02-16 19:11:51 -0700

    Cleaned up relationships

--- lib/load_files.rb
+++ lib/load_files.rb
@@ -9,7 +9,7 @@ require_relative 'customer'
 class LoadFiles
 
   def self.load_invoices_file
-    invoices_file = CSV.open("../data/test_invoices.csv", headers: true)
+    invoices_file = CSV.open("../data/invoices.csv", headers: true)
     invoices = []
 
     invoices_file.each do |row|
@@ -19,7 +19,7 @@ class LoadFiles
   end
 
   def self.load_merchants_file
-    merchants_file = CSV.open("../data/test_merchants.csv", headers: true)
+    merchants_file = CSV.open("../data/merchants.csv", headers: true)
     merchants = []
 
     merchants_file.each do |row|
@@ -29,7 +29,7 @@ class LoadFiles
   end
 
   def self.load_items_file
-    items_file = CSV.open("../data/test_items.csv", headers: true)
+    items_file = CSV.open("../data/items.csv", headers: true)
     items = []
 
     items_file.each do |row|
@@ -39,7 +39,7 @@ class LoadFiles
   end
 
   def self.load_customers_file
-    customers_file = CSV.open("../data/test_customers.csv", headers: true)
+    customers_file = CSV.open("../data/customers.csv", headers: true)
     customers = []
 
     customers_file.each do |row|
@@ -49,7 +49,7 @@ class LoadFiles
   end
 
   def self.load_invoice_items_file
-    invoice_items_file = CSV.open("../data/test_invoice_items.csv", headers: true)
+    invoice_items_file = CSV.open("../data/invoice_items.csv", headers: true)
     invoice_items = []
 
     invoice_items_file.each do |row|
@@ -59,7 +59,7 @@ class LoadFiles
   end
 
   def self.load_transactions_file
-    transactions_file = CSV.open("../data/test_transactions.csv", headers: true)
+    transactions_file = CSV.open("../data/transactions.csv", headers: true)
     transactions= []
 
     transactions_file.each do |row|
@@ -75,32 +75,32 @@ LoadFiles.load_invoices_file
 LoadFiles.load_customers_file
 LoadFiles.load_transactions_file
 LoadFiles.load_invoice_items_file
-puts
-merchant = Merchant.find_by_id(8)
-puts merchant.items
-puts
-merchant = Merchant.find_by_id(27)
-puts merchant.invoices
-puts "******* Rel 12 *******"
-customer = Customer.find_by_id(1)
-puts
-puts customer.invoices
-puts "******* Rel 11 *******"
-transaction = Transaction.find_by_invoice_id(5)
-puts transaction.invoice
-puts "******* Rel 10 *******"
-item = Item.find_by_merchant_id(1)
-puts item.merchant
-puts "******* Rel 9 *******"
-item = Item.find_by_id(1)
-puts item.invoice_items
-puts "******* Rel 8 *******"
-invoice_item = InvoiceItem.find_by_item_id(1)
-puts invoice_item.item
-puts "******* Rel 7 *******"
-invoice_item = InvoiceItem.find_by_invoice_id(5)
-puts invoice_item.invoice
-puts "******* Rel 6 *******"
-invoice = Invoice.find_by_customer_id(1)
-puts invoice.customer
+# puts
+# merchant = Merchant.find_by_id(8)
+# puts merchant.items
+# puts
+# merchant = Merchant.find_by_id(27)
+# puts merchant.invoices
+# puts "******* Rel 12 *******"
+# customer = Customer.find_by_id(29)
+# puts
+# puts customer.invoices
+# puts "******* Rel 11 *******"
+# transaction = Transaction.find_by_id(29)
+# puts transaction.invoice
+# puts "******* Rel 10 *******"
+# item = Item.find_by_id(29)
+# puts item.merchant
+# puts "******* Rel 9 *******"
+# item = Item.find_by_id(29)
+# puts item.invoice_items
+# puts "******* Rel 8 *******"
+# invoice_item = InvoiceItem.find_by_item_id(1)
+# puts invoice_item.item
+# puts "******* Rel 7 *******"
+# invoice_item = InvoiceItem.find_by_invoice_id(5)
+# puts invoice_item.invoice
+# puts "******* Rel 6 *******"
+# invoice = Invoice.find_by_customer_id(1)
+# puts invoice.customer
 
